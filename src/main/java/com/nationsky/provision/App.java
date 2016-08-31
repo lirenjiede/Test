fb4861ed71188e56ef2426d23cff5a74ae289521
393833841@qq.com <lirenjie@nationsky.com>
2016-08-31 23:08:15 +0800

    initial commit comment

---
+++ src/main/java/com/nationsky/provision/App.java
@@ -0,0 +1,13 @@
+package com.nationsky.provision;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
